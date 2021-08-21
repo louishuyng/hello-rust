d53d6123c94a601dcf351e356eee7402e356c854
Louis <huynguyennbk@gmail.com>
2021-08-21 19:05:21 +0700

    shadow variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     // Bad
-    // let x = 5;
-    // println!("The value of x is {}", x);
-    // x = 6;
-    // println!("The value of x is {}", x);
+    let x = 5;
+    println!("The value of x is {}", x);
+    x = 6;
+    println!("The value of x is {}", x);
     
     // Good 
     let mut x = 5;
@@ -14,4 +14,15 @@ fn main() {
     const MAX_POINTS: u32 = 100_000;
     
     println!("The value of MAX_POINTS is {}", MAX_POINTS);
+
+    // Shadow
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+
+    // Bad Shadow
+    let mut spaces = "   ";
+    spaces = spaces.len();
 }
