620ea5280bf1e9d6a37e7a9c5f93b2a8e70a403b
Louis <huynguyennbk@gmail.com>
2021-08-28 21:26:36 +0700

    defin enum values

---
+++ define_enum/src/main.rs
@@ -0,0 +1,30 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+enum IpAddr {
+  V4(u8, u8, u8, u8),
+  V6(String)
+}
+
+#[derive(Debug)]
+enum Message {
+  Quit,  // has no data associated with it at all.
+  Move { x: i32, y: i32 }, // includes an anonymous struct inside it.
+  Write(String), // includes a single String.
+  ChangeColor(i32, i32, i32), // includes three i32 values.
+}
+
+impl Message {
+  fn call(&self) {
+    println!("{:?}", self);
+  }
+}
+
+fn main() {
+  let home = IpAddr::V4(127, 0, 0, 1);
+  let loopback = IpAddr::V6(String::from("::1"));
+
+  let m = Message::Write(String::from("hello"));
+  m.call();
+}
