8b2480ab69178b6cb5719562801d480798a548bb
Louis <huynguyennbk@gmail.com>
2021-08-28 16:17:42 +0700

    method syntax

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -11,6 +11,16 @@ struct Rectangle {
   height: u32,
 }
 
+impl Rectangle {
+  fn area(&self) -> u32 {
+    self.width * self.height
+  }
+
+  fn can_hold(&self, other: &Rectangle) -> bool {
+    self.width > other.width && self.height > other.height
+  }
+}
+
 fn main() {
   let user1 = User {
     username: String::from("louisror"),
@@ -43,11 +53,25 @@ fn main() {
       width: 30,
       height: 50,
   };
+  
+  let rect2 = Rectangle {
+      width: 10,
+      height: 40,
+  };
+  
+  let rect3 = Rectangle {
+      width: 60,
+      height: 45,
+  };
 
   println!("The area of the rectangle is {} square pixels.", area(&rect1));
+  println!("The area of the rectangle is {} square pixels.", rect1.area());
 
   println!("rect1 is {:?}", rect1);
   println!("rect1 is {:#?}", rect1);
+  
+  println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+  println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 fn build_user(email: String, username: String) -> User {
