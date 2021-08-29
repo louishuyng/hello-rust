78ea07ba53abf6890fe5bc49ef184528d220cfdf
Louis <huynguyennbk@gmail.com>
2021-08-29 14:41:53 +0700

    use external packages

--- my_project/restaurant/src/main.rs
+++ my_project/restaurant/src/main.rs
@@ -1,9 +1,13 @@
 use std::fmt::Result;
 use std::io::Result as IoResult;
+use rand::Rng;
 
 fn main() {
   restaurant::eat_at_restaurant();
   restaurant::hosting::add_to_waitlist();
+  
+  let secret_number = rand::thread_rng().gen_range(1..101);
+  println!("secret number is {}", secret_number);
 }
 
 // fn function1() -> Result {
